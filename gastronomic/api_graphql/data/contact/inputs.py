8da9f47520c7b20293232d417ec0f89a3bcf0595
jhayber17 <jhayberm@unicauca.edu.co>
2021-04-26 19:53:24 -0500

    Se agrega el atributo license_plate en Contact/Input

--- gastronomic/api_graphql/data/contact/inputs.py
+++ gastronomic/api_graphql/data/contact/inputs.py
@@ -17,7 +17,7 @@ class CreateContactInput(InputObjectType):
     lastnames = String(required=True)
     location = String(required=True)
     telephone = String(required=True)
-    license = String()
+    license_plate = String()
 
     # Relaciones
     user_id = ID(required=True)
@@ -34,7 +34,7 @@ class UpdateContactInput(InputObjectType):
     lastnames = String()
     location = String()
     telephone = String()
-    license = String()
+    license_plate = String()
 
     # Relaciones
     user_id = ID()
