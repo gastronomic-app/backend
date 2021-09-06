ad448997deda084cd45b280b81aef19a74dacbd0
Cristian Ordoñez <cristiano_ord93@hotmail.com>
2021-09-06 12:52:44 -0500

    Resolve bugs of models

--- gastronomic/profiles/admin.py
+++ gastronomic/profiles/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
+from django.db import models
 
 from .models import UserProfile
 
 # Register your models here.
 
 
-admin.site.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    exclude = ("last_login", )
+    class meta:
+        model = UserProfile
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
