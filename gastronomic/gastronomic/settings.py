22abb59b2e71fda22751449f292427d79e3ac1b1
jlguaca <jlguaca@unicauca.edu.co>
2021-10-09 18:57:40 -0500

    Actualizacion del Back en los archivos enterprises

--- gastronomic/gastronomic/settings.py
+++ gastronomic/gastronomic/settings.py
@@ -193,7 +193,7 @@ if os.getenv('ON_HEROKU'):
 MEDIA_URL = '/Gastronomic/'
 
 CLOUDINARY_STORAGE = {
-     'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
+    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
     'API_SECRET': os.getenv('CLOUDINARY_API_SECRET')
 }
