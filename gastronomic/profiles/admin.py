7bca7e6a3b41e9d033509b5f7ac9c52e944f995a
Cristian Ordoñez <cristiano_ord93@hotmail.com>
2021-10-11 23:26:28 -0500

    Update userprofile for admin

--- gastronomic/profiles/admin.py
+++ gastronomic/profiles/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-
 from django.contrib.auth.admin import UserAdmin
-from .forms import UserChangeForm, UserCreationForm
 
+from .forms import UserChangeForm, UserCreationForm
 from .models import UserProfile
 
 # Register your models here.
@@ -12,21 +11,55 @@ class UserProfileAdmin(UserAdmin):
     add_form = UserCreationForm
     form = UserChangeForm
     model = UserProfile
-    exclude = ("last_login", 'username')
-    list_display = ('email', 'is_active','type','is_staff',)
-    list_filter = ('email', 'is_staff', 'is_active',)
-    fieldsets = (
-        (None, {'fields': ('email', 'password', 'type',)}),
-        ('Permissions', {'fields': ('is_staff', 'is_active','enterprise', 'is_alternative',)}),
+    exclude = ("last_login", "username")
+    list_display = (
+        "email",
+        "is_active",
+        "type",
+        "is_staff",
     )
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2',)}
+    list_filter = (
+        "email",
+        "is_staff",
+        "is_active",
+    )
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "email",
+                    "password",
+                    "type",
+                )
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_staff",
+                    "is_active",
+                    "is_alternative",
+                )
+            },
         ),
     )
-    search_fields = ('email',)
-    ordering = ('email',)
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                ),
+            },
+        ),
+    )
+    search_fields = ("email",)
+    ordering = ("email",)
 
 
 admin.site.register(UserProfile, UserProfileAdmin)
